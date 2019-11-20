a4cd2110fd9aeb162ed331c579aa984cca28aeab
Anita Kemp, Devin Cooley and Neha Malhotra <devin@>
2019-11-20 15:56:57 -0800

    add giphy

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Giphy from '../assets/img/giphy.gif';
 
 function Home(){
+  var style = {
+    height: "600px",
+    marginTop: "30px",
+    border: "solid pink 10px"
+  }
   return(
     <div>
-      <h1>Hello</h1>
+      <img style={style} src={Giphy} alt=""/>
     </div>
   );
 }
